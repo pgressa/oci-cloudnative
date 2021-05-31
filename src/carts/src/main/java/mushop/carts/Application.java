d511c8f869e122fcfe1a7538f974cb9c7c8453e0
Denis Stepanov <denis.stepanov@gmail.com>
2021-05-31 10:36:20 -0500

    Carts native

--- src/carts/src/main/java/mushop/carts/Application.java
+++ src/carts/src/main/java/mushop/carts/Application.java
@@ -1,8 +1,10 @@
 package mushop.carts;
 
+import io.micronaut.core.annotation.TypeHint;
 import io.micronaut.runtime.Micronaut;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
+import org.glassfish.json.JsonProviderImpl;
 
 @OpenAPIDefinition(
         info = @Info(
@@ -10,6 +12,19 @@ import io.swagger.v3.oas.annotations.info.Info;
                 version = "1.0"
         )
 )
+@TypeHint(value = {
+        JsonProviderImpl.class
+}, typeNames = {
+        "oracle.sql.AnyDataFactory",
+        "oracle.sql.Datum",
+        "oracle.sql.TypeDescriptorFactory",
+        "oracle.sql.json.OracleJsonDatum",
+        "oracle.sql.json.OracleJsonFactory",
+        "oracle.sql.json.OracleJsonGenerator",
+        "oracle.sql.json.OracleJsonParser",
+        "oracle.sql.json.OracleJsonValue",
+}, accessType = TypeHint.AccessType.ALL_PUBLIC)
+
 public class Application {
 
     public static void main(String[] args) {
