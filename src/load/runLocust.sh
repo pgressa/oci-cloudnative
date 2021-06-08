45ae9db64ab2e15be2b626b7c4f4aa68d5d93aae
Pavol Gressa <pavol.gressa@oracle.com>
2021-06-08 17:03:39 +0200

    exit locust with error

--- src/load/runLocust.sh
+++ src/load/runLocust.sh
@@ -47,6 +47,10 @@ do_exec() {
 
   echo "Will run $LOCUST_FILE against $TARGET_HOST. Spawning $CLIENTS clients and $RUNTIME total run time."
   locust --host=http://$TARGET_HOST -f $LOCUST_FILE --users=$CLIENTS --spawn-rate=5 --run-time=$RUNTIME --headless --only-summary --exit-code-on-error 1
+  if [[ $? -ne 0 ]] ; then
+    echo "done with failure"
+    exit 1
+  fi
   echo "done"
 }
 
